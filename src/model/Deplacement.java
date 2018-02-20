2d55cc3c32ddf8a2543904776625459460122dd0
Hecq <adrienhecq@gmail.com>
2018-02-20 13:46:10 +0100

    Deplacement

--- src/model/Deplacement.java
+++ src/model/Deplacement.java
@@ -1,6 +1,33 @@
 package model;
 
-public interface Deplacement 
-{
-    
+public interface Deplacement {
+
+    int getX();
+
+    int getY();
+
+    void setX(int x);
+
+    void setY(int y);
+
+    default void deplaceHorizontal(int distance) {
+        setX(getX() + distance);
+    }
+
+    default void deplaceVertical(int distance) {
+        setY(getY() + distance);
+    }
+
+    default void déplacer(Direction d, int distance) {
+        switch (d) {
+            case HAUT:
+                this.deplaceVertical(distance);
+            case BAS:
+                this.deplaceVertical(distance);
+            case GAUCHE:
+                this.deplaceHorizontal(distance);
+            case DROITE:
+                this.deplaceHorizontal(distance);
+        }
+    }
 }
